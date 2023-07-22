6f72e7190e184a57ae36b20faebc6a08ef0c6465
Vladislav Knyshov <inova136@gmail.com>
2023-07-22 22:22:29 +0300

    feat: save issues on server

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,15 @@ import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider
+      toastOptions={{
+        defaultOptions: {
+          position: "bottom-right",
+          duration: 6000,
+          status: "success",
+        },
+      }}
+    >
       <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
--- src/pages/main.page.tsx
+++ src/pages/main.page.tsx
@@ -2,8 +2,41 @@ import { Box, Button, Heading } from "@chakra-ui/react";
 import { DefaultLayout } from "../layouts/default.layout";
 import { Link } from "react-router-dom";
 import { CreateIssueForm } from "../components/create-issue-form";
+import { CreateIssueDto } from "../dto/create-issue.dto";
+import { validateCaptcha } from "../api/validate-captcha.api";
+import { createIssue } from "../api/create-issue.api";
+import { useToast } from "@chakra-ui/react";
 
 export function MainPage() {
+  const toast = useToast();
+
+  const handleSubmitIssue = async (
+    createIssueDto: CreateIssueDto,
+    captchaResponse: string
+  ) => {
+    try {
+      const captchaValidationResponse = await validateCaptcha(captchaResponse);
+      const captchaPassed = captchaValidationResponse.data.success;
+      if (!captchaPassed) throw new Error("Captcha validation failed");
+
+      const issueCreatedResponse = await createIssue(createIssueDto);
+      const issueCreated = issueCreatedResponse.status === 201;
+      if (!issueCreated) throw new Error("Issue creation failed");
+
+      toast({
+        title: "Issue created",
+      });
+      return true;
+    } catch (e) {
+      toast({
+        title: "Some error occured",
+        status: "error",
+        duration: 9000,
+      });
+      return false;
+    }
+  };
+
   return (
     <DefaultLayout
       navigation={
@@ -15,7 +48,7 @@ export function MainPage() {
       }
     >
       <Heading paddingBottom={8}>Submit an issue</Heading>
-      <CreateIssueForm onSubmit={console.log} />
+      <CreateIssueForm onSubmit={handleSubmitIssue} />
     </DefaultLayout>
   );
 }
