cbf33aacbda5fad32ca4fbbd50d8fca0cb9a1620
Vladislav Knyshov <inova136@gmail.com>
2023-07-22 19:18:34 +0300

    feat: add recapcha types & site-key

--- src/pages/main.page.tsx
+++ src/pages/main.page.tsx
@@ -8,6 +8,7 @@ import {
 } from "@chakra-ui/react";
 import { DefaultLayout } from "../layouts/default.layout";
 import { Link } from "react-router-dom";
+import ReCAPTCHA from "react-google-recaptcha";
 
 export function MainPage() {
   return (
@@ -21,11 +22,15 @@ export function MainPage() {
       }
     >
       <Heading paddingBottom={8}>Submit an issue</Heading>
-      <VStack spacing={4} width="100%">
+      <VStack spacing={4} width="100%" align="flex-start">
         <Input placeholder="Your first name" />
         <Input placeholder="Your last name" />
         <Input placeholder="Your email" />
         <Textarea rows={10} placeholder="Your message" />
+        <ReCAPTCHA
+          sitekey={import.meta.env.VITE_SITE_KEY}
+          onChange={console.log}
+        />
         <Box display="flex" justifyContent="flex-end" width="100%">
           <Button variant="solid" colorScheme="teal">
             Submit
